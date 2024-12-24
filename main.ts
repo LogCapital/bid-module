5f22633d50026ee54c3c74f74232e74bb28aba61
Igor Suvorov <suekandig@mail.ru>
2024-12-24 19:48:20 +0300

    Переделает main.ts с использованием ConfigService

--- main.ts
+++ main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { OpenTelemetryNodeSDK } from 'src/tracing';
+import { ConfigService, ConfigType } from '@nestjs/config';
+import { appConfig, natsConfig } from 'src/config/config';
+import { Logger } from '@nestjs/common';
+import { NatsOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
+  OpenTelemetryNodeSDK.start();
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const logger = new Logger();
+
+  const configService = app.get(ConfigService);
+  const configApp: ConfigType<typeof appConfig> = configService.get('app');
+  const configNats: ConfigType<typeof natsConfig> = configService.get('nats');
+
+  app.connectMicroservice<NatsOptions>({
+    transport: Transport.NATS,
+    options: {
+      servers: [`nats://${configNats.host}:${configNats.port}`]
+    }
+  });
+
+  await app.startAllMicroservices();
+  await app.listen(configApp.port, () => {
+    logger.log(`App is listening on ${configApp.port}`);
+  });
 }
 bootstrap();
