f3156f66f04729892871d1548bf07db51ea1f1d8
Igor Suvorov <suekandig@mail.ru>
2024-12-27 19:04:00 +0300

    Создаст entity Contract

---
+++ src/domain/typeorm/contract.entity.ts
@@ -0,0 +1,43 @@
+import {
+    Column,
+    CreateDateColumn,
+    Entity,
+    OneToMany,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn
+} from "typeorm";
+import { Bid } from "./bid.entity";
+import { CONTRACT_STATUS } from "src/common/enums/contract-status.enum";
+
+@Entity()
+export class Contract {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column('uuid', {
+        name: 'user_id',
+    })
+    userId: string;
+
+    @Column('enum', {
+        name: 'contract_status',
+        enumName: 'CONTRACT_STATUS',
+        enum: CONTRACT_STATUS,
+    })
+    contractStatus: CONTRACT_STATUS;
+
+    @CreateDateColumn({
+        name: 'created_at',
+        type: 'timestamp',
+    })
+    createdAt: Date;
+
+    @UpdateDateColumn({
+        name: 'updated_at',
+        type: 'timestamp',
+    })
+    updatedAt: Date;
+
+    @OneToMany(() => Bid, bid => bid.contract)
+    bids: Bid[];
+}
